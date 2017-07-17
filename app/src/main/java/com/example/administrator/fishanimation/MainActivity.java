a105ea070661674b0013c186aea20115ff89821b
zspxsx <zspxsx@126.com>
2017-07-17 18:00:27 +0800

    绘制躯体。

--- app/src/main/java/com/example/administrator/fishanimation/MainActivity.java
+++ app/src/main/java/com/example/administrator/fishanimation/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.administrator.fishanimation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);// 去掉标题栏
         setContentView(R.layout.activity_main);
+        ImageView ivFish = findViewById(R.id.iv_fish);
+        ivFish.setImageDrawable(new FinishDrawable());
     }
 }
